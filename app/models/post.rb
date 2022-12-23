8d4c045666fe23b65dfeac39bcb7797dd2faeb06
Mike <55577@pm.me>
2022-12-23 18:16:03 +0000

    Add SEO Friendly urls with friendly_id gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  extend FriendlyId
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true
   belongs_to :user
@@ -10,4 +11,10 @@ class Post < ApplicationRecord
   has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
   has_noticed_notifications model_name: 'Notification'
   has_many :notifications, through: :user
+
+  friendly_id :title, use: %i[slugged history finders]
+
+  def should_generate_new_friendly_id?
+    title_changed? || slug.blank?
+  end
 end
