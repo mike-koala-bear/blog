600ee8300adcd826e69499de098881013333ccb5
Mike <55577@pm.me>
2022-12-24 12:03:07 +0000

    Fix admin panel and recheck site

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,6 +10,6 @@ class AdminController < ApplicationController
   def users; end
 
   def show_post
-    @post = Post.includes(:user, :comments).find(params[:id])
+    @post = Post.includes(:user, comments: [:user, :rich_text_body]).find(params[:id])
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @query = Post.ransack(params[:q])
-    @posts = @query.result(distinct: true)
+    @posts = @query.result(distinct: true).includes(:user, :rich_text_body)
     # .page(params[:page]).per(10)
   end
 end
